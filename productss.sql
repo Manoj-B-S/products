1e794065cf61c92150d17ada11cae8f13a94d40a
Manoj-B-S <bsmanoj125@gmail.com>
2025-07-31 11:35:37 +0530

    edited script file

--- productss.sql
+++ productss.sql
@@ -17,4 +17,36 @@ SHOW VARIABLES LIKE 'local_infile';
 SELECT * FROM products LIMIT 5;
 
 
- 
+-- Checking null  
+SELECT * FROM products
+WHERE id IS NULL
+   OR cost IS NULL
+   OR category IS NULL
+   OR name IS NULL
+   OR brand IS NULL
+   OR retail_price IS NULL
+   OR department IS NULL
+   OR sku IS NULL
+   OR distribution_center_id IS NULL;
+   
+-- Check for Negative Prices or Cost
+ SELECT * FROM products
+WHERE cost < 0 OR retail_price < 0;
+
+-- Check for Duplicates by id or sku
+SELECT id, COUNT(*) AS count
+FROM products
+GROUP BY id
+HAVING COUNT(*) > 1;
+
+SELECT sku, COUNT(*) AS count
+FROM products
+GROUP BY sku
+HAVING COUNT(*) > 1;
+
+-- Count Products per Category
+SELECT category, COUNT(*) AS product_count
+FROM products
+GROUP BY category
+ORDER BY product_count DESC;
+
